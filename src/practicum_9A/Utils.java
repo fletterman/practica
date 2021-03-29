f5883c92acd17b031acde972cfe45bc40ddcb592
fletterman <stijn.castrop@me.com>
2021-03-29 15:00:22 +0200

    Update klassediagrammen, merge-conflict en utils-class

--- src/practicum_9A/Utils.java
+++ src/practicum_9A/Utils.java
@@ -4,21 +4,29 @@ import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 
 public class Utils {
+    private static int count = 0;
+
     public static void main(String[] args){
         System.out.println(euroBedrag(3.2222));
         System.out.println(euroBedrag(3.64286268, 3));
+        System.out.println(count);
     }
 
     public static String euroBedrag(double bedrag){
-        return "€" + new DecimalFormat("##.##").format(bedrag);
+        return euroBedrag(bedrag, 2);
     }
 
     public static String euroBedrag(double bedrag, int precisie){
+        count++;
         String test = String.valueOf(bedrag);
         int split = test.indexOf(".");
         String eind = test.substring(split + 1);
         String begin = test.substring(0, split);
         String nieuwEind = eind.substring(0, precisie);
         return "€" + begin + "," + nieuwEind;
+    }
+
+    public static int getCount() {
+        return count;
     }
 }
