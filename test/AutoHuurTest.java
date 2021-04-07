cac74a1b0840db92b113101e36b3c219a54d2e6d
fletterman <stijn.castrop@me.com>
2021-04-07 21:45:47 +0200

    aanpassingen test autohuur

--- test/AutoHuurTest.java
+++ test/AutoHuurTest.java
@@ -6,6 +6,11 @@ import practicum_4B.*;
 
 public class AutoHuurTest {
     private AutoHuur autoHuur;
+    private Klant klant;
+    private Klant klantNull = null;
+    private Auto auto;
+    private Auto autoNull = null;
+
     @BeforeEach
     public void init(){
         autoHuur = new AutoHuur();
@@ -23,4 +28,14 @@ public class AutoHuurTest {
         autoHuur.getHuurder().setKorting(5);
         assertEquals("test (korting: 5%)", autoHuur.getHuurder().toString());
     }
+
+    //Alle tests zullen vanaf hier falen/exception gooien omdat ik deze situaties al in de code opvang. Een autohuur mag namelijk niet bestaan zonder een auto of/en zonder een klant.
+    @Test
+    void setAuto(){
+        autoHuur.setHuurder(klantNull);
+        assertEquals("er is geen auto bekend\ner is geen huurder bekend\naantal dagen: 0 en dat kost 0.0", "test");
+    }
+
+    @Test
+    void setKlantNull
 }
